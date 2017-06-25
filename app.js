552522fcbd4d055458e5754b70dc923e6cd14b78
tkeesling <tyler.keesling@gmail.com>
2017-06-25 10:13:48 -0600

    added json

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
-	// sidebar nav collapse
-	$(".button-collapse").sideNav();
-})
+	// button click functionality
+	$('.btn').click(function(e) {
+		e.preventDefault
+		// capture values from input form
+		var sex = $('input[name=sex]:checked').val()
+		var age = $('#age').val()
+		var height = $('#height').val()
+		var weight = $('#weight').val()
+
+		// attach values to local JSON
+		$.getJSON('/values.json', function(data) {
+			var obj = data
+			obj.sex = sex
+			obj.age = age
+			obj.weight.value = weight
+			obj.height.value = height
+		}) // end of getJSON
+	}) // end of btn click
+}) // end of doc ready
